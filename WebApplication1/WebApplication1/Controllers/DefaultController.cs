ab52f7ecac92ffb0c60e5ff49e20f58ed01a4974
daniel880830 <daniel880830@gmail.com>
2020-08-09 16:53:31 +0800

    first

---
+++ WebApplication1/WebApplication1/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            Models.students student1 = new Models.students();
+            student1.ID = "1";
+            ViewBag.name = "ethan";
+            return View();
+        }
+    }
+}
